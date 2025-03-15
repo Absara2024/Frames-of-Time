968d3122328a0e30936c277cb04cb24aa7b1c4a3
ABSARA <absara_2021@yahoo.com>
2025-03-14 18:30:57 -0700

    initial code

--- vite-project/src/components/users.jsx
+++ vite-project/src/components/users.jsx
@@ -32,5 +32,40 @@ function Users() {
     </div>
   );
 }
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3012/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the users!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user._id}>
+            {user.name} - {user.graduateYear}
+            <ul>
+              {user.schools.map(school => (
+                <li key={school._id}>{school.name}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 
 export default Users;
