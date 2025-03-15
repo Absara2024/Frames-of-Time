45ddddb789a0c7eee346cc501a210ad995915ea7
ABSARA <absara_2021@yahoo.com>
2025-03-14 18:00:11 -0700

    initial code

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3012;
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/Frames of Time')
+mongoose.connect('mongodb://localhost/Frames of Time')
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => {
     console.error('MongoDB connection error:', error);
